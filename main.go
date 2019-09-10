a16afb99325257cf8927c4c06d33641b666597cd
Jared Kobos <jaredkobos@gmail.com>
2019-09-10 19:04:11 -0400

    Initial working API

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type feedback struct {
+	ID         		 int64    `json:"id"`
+	Feedback       string `json:"feedback"`
+}
+
+type allFeedback []feedback 
+
+var feedbackData = allFeedback{
+	{
+		ID:          1,
+		Feedback: "This is some sample feedback",
+	},
+}
+
+func getFeedbackItems(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(feedbackData)
+}
+
+func createFeedback(w http.ResponseWriter, r *http.Request) {
+	var newFeedback feedback
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "API Error: incorrect data format")
+	}
+	err = json.Unmarshal(reqBody, &newFeedback)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(newFeedback.Feedback)
+	feedbackData = append(feedbackData, newFeedback)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newFeedback)
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/feedback", getFeedbackItems).Methods("GET")
+	router.HandleFunc("/feedback", createFeedback).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
