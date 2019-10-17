fafb190966a43f653cc5c6bcbf95874976a5d115
Jared Kobos <jaredkobos@gmail.com>
2019-10-17 10:25:27 -0400

    Add Cors headers

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func getFeedbackItems(w http.ResponseWriter, r *http.Request) {
 }
 
 func createFeedback(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
 	var newFeedback feedback
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -43,6 +44,12 @@ func createFeedback(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 
 	json.NewEncoder(w).Encode(newFeedback)
+}
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func main() {
